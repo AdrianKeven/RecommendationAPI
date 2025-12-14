e89fef0f7daa6aab726e3e5e3d85842db1e22a5f
AdrianKeven <adrian.kevenas@gmail.com>
2025-12-14 01:30:14 -0300

    Initial Spring Boot project with movie recommendation API

---
+++ src/test/java/com/adriankdev/RecommendationAPI/RecommendationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriankdev.RecommendationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
